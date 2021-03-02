9872003c71fdfa8184e90b09eb6090825f389780
Abdallah Altokhy <abdallahaltokhy@gmail.com>
2021-03-02 20:02:58 +0100

    Hass customer passwords

--- server/controller/customer-controller.js
+++ server/controller/customer-controller.js
@@ -1,10 +1,16 @@
 const dbConnection = require('../db');
+const bcrypt = require('bcrypt');
 
-const registerUser = (req, res) => {
+
+const registerUser = async (req, res) => {
 
   let sql = `INSERT INTO customers(customer_name, location , phone ,email ,customer_password) VALUES (?)`;
-
   let { customer_name, location, phone, email, customer_password } = req.body;
+
+  //hash the password
+  const salt = await bcrypt.genSalt(10);
+  customer_password = await bcrypt.hash(customer_password, salt);
+
   let values = [customer_name, location, phone, email, customer_password];
 
   dbConnection.query(sql, [values], function (err, data, fields) {
