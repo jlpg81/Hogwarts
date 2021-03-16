8ab84c022aa526c434f741201088e3223c9768bd
Salvatore Pariota <salpariota@gmail.com>
2021-03-16 14:40:45 +0100

    added test that mocks local storage at login

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
+// import axios from "axios";
 import App from "./App";
-// import AddressForm from "./Checkout/AddressForm";
-// import userEvent from "@testing-library/user-event";
-// import mockUser from "./mocks";
+import { mockUser } from "./mocks";
+
+jest.mock("axios");
 
 test("should render app children", () => {
   render(<App />);
@@ -12,8 +14,39 @@ test("should render app children", () => {
   ).toBeInTheDocument();
 });
 
-test("trying something from Mo's demo", () => {
+// test("trying something from Mo's demo", () => {
+//   render(<App />);
+//   const text = screen.getByText("Home maintenance made easier and better");
+//   expect(text).toBeInTheDocument();
+// });
+
+it("Should retrieve JWT token on initiation", () => {
+  Object.defineProperty(window, "localStorage", {
+    value: {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      removeItem: jest.fn(),
+    },
+    writable: true,
+  });
+
+  jest.mock("axios");
   render(<App />);
-  const text = screen.getByText("Home maintenance made easier and better");
-  expect(text).toBeInTheDocument();
+  expect(localStorage.getItem).toHaveBeenCalled();
 });
+
+//this is WIP
+// it("Should retrieve JWT token on initiation", () => {
+//   Object.defineProperty(window, "localStorage", {
+//     value: {
+//       getItem: jest.fn(),
+//       setItem: jest.fn(),
+//       removeItem: jest.fn(),
+//     },
+//     writable: true,
+//   });
+//   const data = { mockUser }
+
+//   render(<App />);
+//   axios.get
+// });
